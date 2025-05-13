d81d7856115ad2c7e3e121e218575bea42e4c9d6
ocarson1 <owen_carson@brown.edu>
2025-05-13 14:31:36 -0400

    hide

--- src/Gallery.js
+++ src/Gallery.js
@@ -7,12 +7,14 @@ import { parseCustomDateString, formatDayDate, formatTime } from './utils/dateUt
 
 const Gallery = ({ images }) => {
 
-
-
 const [selectedIndex, setSelectedIndex] = useState(null);
 
+// Filter out images with hide=true tag
+const visibleImages = images.filter(image => 
+  !(image.metadata.hide === "true")
+);
 
-const reversedImages = [...images].reverse();
+const reversedImages = [...visibleImages].reverse();
 
   const handleImageClick = (index) => {
     // Convert the reversed index to the actual index
@@ -26,74 +28,66 @@ const reversedImages = [...images].reverse();
 
   const handlePrevious = (e) => {
     e.stopPropagation();
-    setSelectedIndex((prev) => (prev < images.length - 1 ? prev + 1 : 0));
-
+    setSelectedIndex((prev) => (prev < visibleImages.length - 1 ? prev + 1 : 0));
   };
 
   const handleNext = (e) => {
     e.stopPropagation();
-    setSelectedIndex((prev) => (prev > 0 ? prev - 1 : images.length - 1));
-
+    setSelectedIndex((prev) => (prev > 0 ? prev - 1 : visibleImages.length - 1));
   };
 
     console.log('gallery')
-    console.log(images)
-
+    console.log(visibleImages)
 
     return (
         <div className="gallery-section">
-                    <div className="gallery-container">
-                    <h3 style={{ maxWidth: '1080px', fontWeight: "bold" }}>Gallery</h3><br></br>
+            <div className="gallery-container">
+                <h3 style={{ maxWidth: '1080px', fontWeight: "bold" }}>Gallery</h3><br></br>
 
-        <div className="gallery-grid">
+                <div className="gallery-grid">
+                    {reversedImages.map((image, index) => (
+                        <div className="grid-item" key={index} onClick={() => handleImageClick(index)}>
+                            <div className="gal-image-wrapper">
+                                <img src={image.imageUrl} alt={image.metadata.title} />
+                            </div>
+                            <div className="caption" style={{width: '100%',textAlign: 'left'}}>
+                                <h3 style={{width: '100%', textAlign: 'left'}}>{formatDayDate(parseCustomDateString(image.metadata.title))}</h3>
+                                <h3 style={{width: '100%', textAlign: 'left', fontWeight:'400'}}>{formatTime(parseCustomDateString(image.metadata.title))}</h3>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                {selectedIndex !== null && (
+                <div className="modal-overlay" onClick={handleClose}>
+                    <div className="modal-content">
+                        {/* Close button */}
+                        <button className="close-button" onClick={handleClose}>
+                            <X size={24} />
+                        </button>
 
-        {reversedImages.map((image, index) => (
-            
-                <div className="grid-item" key={index} onClick={() => handleImageClick(index)}>
-                    <div className="gal-image-wrapper">
-                    <img src={image.imageUrl} alt={image.metadata.title} />
-                    </div>
-                    <div className="caption" style={{width: '100%',textAlign: 'left'}}>
-                      
-                        <h3 style={{width: '100%', textAlign: 'left'}}>{formatDayDate(parseCustomDateString(image.metadata.title))}</h3>
-                        <h3 style={{width: '100%', textAlign: 'left', fontWeight:'400'}}>{formatTime(parseCustomDateString(image.metadata.title))}</h3>
+                        {/* Navigation buttons */}
+                        <button className="nav-button nav-button-left" onClick={handlePrevious}>
+                            <ChevronLeft size={40} />
+                        </button>
+                        <button className="nav-button nav-button-right" onClick={handleNext}>
+                            <ChevronRight size={40} />
+                        </button>
+
+                        {/* Main image */}
+                        {console.log("modal", visibleImages[selectedIndex])}
+                        <img
+                            src={visibleImages[selectedIndex].imageUrl}
+                            alt={visibleImages[selectedIndex].alt}
+                            className="modal-image"
+                            onClick={(e) => e.stopPropagation()}
+                        />
                     </div>
                 </div>
-            ))}
+                )}
+                <br></br>
+                <br></br>
+            </div>
         </div>
-        {selectedIndex !== null && (
-        <div className="modal-overlay" onClick={handleClose}>
-          <div className="modal-content">
-            {/* Close button */}
-            <button className="close-button" onClick={handleClose}>
-              <X size={24} />
-            </button>
-
-            {/* Navigation buttons */}
-            <button className="nav-button nav-button-left" onClick={handlePrevious}>
-              <ChevronLeft size={40} />
-            </button>
-            <button className="nav-button nav-button-right" onClick={handleNext}>
-              <ChevronRight size={40} />
-            </button>
-
-            {/* Main image */}
-            {console.log("modal", images[selectedIndex])}
-            <img
-              src={images[selectedIndex].imageUrl}
-              alt={images[selectedIndex].alt}
-              className="modal-image"
-              onClick={(e) => e.stopPropagation()}
-            />
-          </div>
-        </div>
-      )}
-        <br>
-        </br>
-        <br></br>
-        </div>
-        </div>
-      
     )
 };
 
@@ -108,6 +102,7 @@ Gallery.propTypes = {
                 title: PropTypes.string.isRequired,
                 description: PropTypes.string.isRequired,
                 uploadDate: PropTypes.string.isRequired,
+                hide: PropTypes.string, // Added hide prop type
             }).isRequired,
         })
     ).isRequired,
